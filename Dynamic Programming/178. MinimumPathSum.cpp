4733ff64142d8f7aaf9b128880d58a6beed04f30
Navya <75139913+rajputnavya@users.noreply.github.com>
2023-06-07 17:00:51 +0530

    Create 178. MinimumPathSum.cpp

---
+++ Dynamic Programming/178. MinimumPathSum.cpp
@@ -0,0 +1,51 @@
+// Question Link :
+// Code Studio : https://www.codingninjas.com/codestudio/problems/minimum-path-sum_8230791?challengeSlug=striver-sde-challenge
+// Leetcode : 
+
+// APPROACH 1 - MEMOIZATION
+// TC : O(m*n)
+// SC : O(m*n)
+
+#include <bits/stdc++.h> 
+int minSumPath(vector<vector<int>> &grid, int m, int n, int i, int j, int **output){
+    // Base Case
+    if(i == m-1 && j == n-1){
+        return grid[i][j];
+    }
+    // Edge Case
+    if(i >= m || j >= n){
+        return INT_MAX;
+    }
+    // Checking if value already exists
+    if(output[i][j] != -1){
+        return output[i][j];
+    }
+    // Calculate ans save value if not exists
+    int x = minSumPath(grid, m, n, i, j+1, output);
+    int y = minSumPath(grid, m, n, i+1, j, output);
+    output[i][j] = min(x, y) + grid[i][j];
+    return output[i][j];
+}
+int minSumPath(vector<vector<int>> &grid) {
+    // Write your code here.
+    int m = grid.size(); // rows
+    int n = grid[0].size(); //columns
+    // Output array
+    int **output = new int *[m];
+    for(int i=0; i<m; i++){
+        output[i] = new int[n];
+    }
+    // Initialising output array with -1
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            output[i][j] = -1;
+        }
+    }
+    return minSumPath(grid, m, n, 0, 0, output);
+}
+
+
+// APPROACH 2 (Optimised) - DP
+
+
+
